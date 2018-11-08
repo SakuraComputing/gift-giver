32a93ddff84210d92be02581e937aa29b006e82b
Ewan Bauld <ewan.bauld@gmail.com>
2018-11-08 14:55:34 +0000

    Add list and passing test

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -6,4 +6,13 @@ const app = shallow(<App />);
 
 it('should render correctly', () => {
     expect(app).toMatchSnapshot();
-})
+});
+
+it('initialises the `state` with an empty list of gifts', () => {
+    expect(app.state().gifts).toEqual([]);
+});
+
+it('add a new gift to `state` when clicking the `add gift` button', () => {
+    app.find('.btn-add').simulate('click');
+    expect(app.state().gifts).toEqual([{ id: 1 }]);
+});
