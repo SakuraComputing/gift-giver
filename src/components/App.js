67ddb3950f060d94d939595d7b688b0a6565978c
Ewan Bauld <ewan.bauld@gmail.com>
2018-11-09 14:30:18 +0000

    Revise BDD

--- src/components/App.js
+++ src/components/App.js
@@ -18,12 +18,23 @@ class App extends Component {
         const max_id = ids.length > 0 ?  Math.max(...ids) : 0;
 
         gifts.push({ id: max_id + 1 })
+        
+        this.setState({ gifts });
     }
 
     render() {
         return (
             <div>
                 <h2>Gift Giver</h2>
+                <div className='gift-list'>
+                    {
+                        this.state.gifts.map(gift => {
+                            return (
+                                <div key={gift.id}>{gift}</div>
+                            )
+                        })
+                    }
+                </div>
                 <Button className='btn-add' onClick={this.addGift}>Add Gift</Button>
             </div>   
         )
--- src/components/App.test.js
+++ src/components/App.test.js
@@ -12,7 +12,21 @@ it('initialises the `state` with an empty list of gifts', () => {
     expect(app.state().gifts).toEqual([]);
 });
 
-it('add a new gift to `state` when clicking the `add gift` button', () => {
-    app.find('.btn-add').simulate('click');
-    expect(app.state().gifts).toEqual([{ id: 1 }]);
+describe('when clicking the `add gift` button', () => {
+
+    beforeEach(() => {
+        app.find('.btn-add').simulate('click');
+    });
+
+    afterEach(() => {
+        app.setState({ gifts: []});
+    });
+
+    it('add a new gift to `state`', () => {
+        expect(app.state().gifts).toEqual([{ id: 1 }]);
+    });
+    
+    it('adds a new gift to the rendered list', () => {
+        expect(app.find('.gift-list').children().length).toEqual(1);
+    });
 });
